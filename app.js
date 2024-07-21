61a6f62a7e61a8392c914742e4833ba49774c9f7
Babanfada <apatapiti@protonmail.com>
2024-07-21 15:56:39 +0100

    setting up express server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+require("dotenv").config();
+require("express-async-errors");
+// server
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5003;
 
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
